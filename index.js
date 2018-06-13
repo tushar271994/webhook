ee746a032269f005d7edad04cb7bbecf9dd032d2
PARTH-THINK\jesus <build@finuno.com>
2018-06-13 11:49:24 +0530

    all

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res, next) {
+  var speech=
+  req.body.result &&
+  req.body.result.parameters &&
+  req.body.result.parameters.Quantity
+  return res.json({
+    speech: "you enter a quantity "+ Quantity
+   });
+});
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+
