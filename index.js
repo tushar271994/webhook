907192fbe48db19dedb6979084b8b53a41bcc5ea
PARTH-THINK\jesus <build@finuno.com>
2018-06-13 13:09:26 +0530

    first

--- index.js
+++ index.js
@@ -17,17 +17,19 @@ restService.post("/echo", function(req, res, next) {
   phoneno='';
   if (req.body.result.parameters.name){
     name = req.body.result.parameters.name;
-  
   }
   if (req.body.result.parameters.address){
     address = req.body.result.parameters.address;
-  
   }
   if (req.body.result.parameters.phoneno){
     phoneno = req.body.result.parameters.phoneno;
   }
-  return res.json({
-    speech: "Hi"+ name + "your address" + address + "and your phone no" + phoneno
+  console.log("name is" , name);
+  console.log("address is" , address);
+  console.log("phoneno is" , phoneno);
+
+return res.json({
+    speech: "Hi"+ name + "your address " + address + "and your phone no" + phoneno
    });
 });
 restService.listen(process.env.PORT || 8000, function() {
