ec04af4bf3c142a6fac089c02616fdb6bbd4e73a
PARTH-THINK\jesus <build@finuno.com>
2018-06-18 12:13:10 +0530

    efew

--- index.js
+++ index.js
@@ -12,36 +12,36 @@ restService.use(
     })
   );
 
-// var Pool=require("pg").Pool;
+var Pool=require("pg").Pool;
 
-// var config = {
-//   user: 'postgres',
-//   database: 'test', 
-//   password: 'tushar123', 
-//   port: 5433, 
-//   host:'localhost'
+var config = {
+  user: 'postgres',
+  database: 'test', 
+  password: 'tushar123', 
+  port: 5433, 
+  host:'localhost'
  
-// };
-// var  Pool = new Pool(config);
+};
+var  Pool = new Pool(config);
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res, next) {
+restService.post("/pizzabot", function(req, res, next) {
     var name = req.body.result.parameters.name
     var address = req.body.result.parameters.address;
 
-    // Pool.connect(function(err, client, done) {
-    //     if (err) {
-    //       return next(err)
-    //     }
-    //     client.query('insert into demo(name,address)  values($1, $2)',
-    //     [username, useraddress], function (err, result) {
-    //       done() 
-    //      if (err) {
-    //         return next(err)
-    //       }
-    //      res.status(200);
-    //     })
-    //   })
+    Pool.connect(function(err, client, done) {
+        if (err) {
+          return next(err)
+        }
+        client.query('insert into demo(name,address)  values($1, $2)',
+        [username, useraddress], function (err, result) {
+          done() 
+         if (err) {
+            return next(err)
+          }
+         res.status(200);
+        })
+      })
       return res.json({
       speech: "Hi"+ " "+ name + " " + ", we will deliver at your address" + " "+ address + " " + "shortly" 
       });
