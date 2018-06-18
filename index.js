0a1560ee5cee4e249fe79f9ca44c0d2e8aea3d70
PARTH-THINK\jesus <build@finuno.com>
2018-06-18 11:52:44 +0530

    efwe

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-
-
 "use strict";
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var pg = require("pg");
 const restService = express();
 
 restService.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-  );
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-// var Pool=require("pg").Pool;
-
-// var config = {
-//   user: 'postgres',
-//   database: 'test', 
-//   password: 'tushar123', 
-//   port: 5433, 
-//   host:'localhost'
- 
-// };
-// var  Pool = new Pool(config);
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res, next) {
-    var name = req.body.name;
-    var address = req.body.address;
+restService.post("/echo", function(req, res) {
+  var name = req.body.name;
+  return res.json({
+    speech: "Hi" + name
+  });
+});
 
-    // Pool.connect(function(err, client, done) {
-    //     if (err) {
-    //       return next(err)
-    //     }
-    //     client.query('insert into demo(name,address)  values($1, $2)',
-    //     [username, useraddress], function (err, result) {
-    //       done() 
-    //      if (err) {
-    //         return next(err)
-    //       }
-    //      res.status(200);
-    //     })
-    //   })
-      return res.json({
-      speech: "Hi"+ " "+ name + " " + ", we will deliver at your address" + " "+ address + " " + "shortly" 
-      });
- });
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
-
-
-
-
