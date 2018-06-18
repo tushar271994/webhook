f1191c17ad83c372a9699c96483aa38b64bcfca5
PARTH-THINK\jesus <build@finuno.com>
2018-06-18 11:13:10 +0530

    second

--- index.js
+++ index.js
@@ -27,19 +27,19 @@ restService.post("/pizzabot", function(req, res, next) {
     var username = req.body.name;
     var useraddress = req.body.address;
 
-    Pool.connect(function(err, client, done) {
-        if (err) {
-          return next(err)
-        }
-        client.query('insert into demo(name,address)  values($1, $2)',
-        [username, useraddress], function (err, result) {
-          done() 
-         if (err) {
-            return next(err)
-          }
-         res.status(200);
-        })
-      })
+    // Pool.connect(function(err, client, done) {
+    //     if (err) {
+    //       return next(err)
+    //     }
+    //     client.query('insert into demo(name,address)  values($1, $2)',
+    //     [username, useraddress], function (err, result) {
+    //       done() 
+    //      if (err) {
+    //         return next(err)
+    //       }
+    //      res.status(200);
+    //     })
+    //   })
       return res.json({
       speech: "Hi"+ " "+ username + " " + ", we will deliver at your address" + " "+ useraddress + " " + "shortly" 
       });
