f96f5395ce576c0544cc17fc0f671e51484d9732
PARTH-THINK\jesus <build@finuno.com>
2018-06-18 10:57:00 +0530

    first

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var pg = require("pg");
 const restService = express();
 
 restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
 
+var Pool=require("pg").Pool;
+
+var config = {
+  user: 'postgres',
+  database: 'test', 
+  password: 'tushar123', 
+  port: 5433, 
+  host:'localhost'
+ 
+};
+var  Pool = new Pool(config);
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res, next) {
-    var username = req.body.result.parameters.name;
-    var useraddress = req.body.result.parameters.address;
-    return res.json({
-    speech: "Hi"+ " "+ username + " " + ", we will deliver at your address" + " "+ useraddress + " " + "shortly" 
-   });
-});
+restService.post("/pizzabot", function(req, res, next) {
+    var username = req.body.name;
+    var useraddress = req.body.address;
+
+    Pool.connect(function(err, client, done) {
+        if (err) {
+          return next(err)
+        }
+        client.query('insert into pizzaorder(name,address)  values($1, $2)',
+        [username, useraddress], function (err, result) {
+          done() 
+         if (err) {
+            return next(err)
+          }
+         res.status(200);
+        })
+      })
+      return res.json({
+      speech: "Hi"+ " "+ username + " " + ", we will deliver at your address" + " "+ useraddress + " " + "shortly" 
+      });
+ });
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
+
+
 
 
